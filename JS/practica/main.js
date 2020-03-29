d15f8267318758a4076665e33bc8d1cc0a3d684a
Kefir <staskefir99@gmail.com>
2020-03-29 10:31:52 +0300

    added new files

---
+++ JS/practica/main.js
@@ -0,0 +1,32 @@
+
+// 6
+// function m(a,b) {
+// //     mul (a,b);
+//     console.log(mul(a,b));
+//     function mul (k,c) {
+//         return  k*c;
+//     }
+// }
+
+// m(2,5);
+
+
+// 7
+// function operation (m,n,o) {
+// console.log(m+n+o);
+// }
+// let srt = function(a,b) {
+//     return a+b;
+// };
+// operation(2, 3,srt(2,1));
+
+// 8
+// function addN(n){
+//     return newAdd(2,n);
+//     function newAdd(a,b){
+//         console.log(a+b);
+//     }
+// }
+// addN(4);
+
+    
